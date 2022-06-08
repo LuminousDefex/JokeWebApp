1b579cb1f0ae2c2096d5013d73858c87bca77e35
LuminousDefex <defex84@gmail.com>
2022-06-08 06:48:43 +0300

    final commit

--- JokeWebApp/Controllers/JokesController.cs
+++ JokeWebApp/Controllers/JokesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JokeWebApp.Data;
 using JokeWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JokeWebApp.Controllers
 {
@@ -58,6 +59,8 @@ namespace JokeWebApp.Controllers
         }
 
         // GET: Jokes/Create
+
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -66,6 +69,7 @@ namespace JokeWebApp.Controllers
         // POST: Jokes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -80,6 +84,7 @@ namespace JokeWebApp.Controllers
         }
 
         // GET: Jokes/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Joke == null)
@@ -98,6 +103,7 @@ namespace JokeWebApp.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -131,6 +137,7 @@ namespace JokeWebApp.Controllers
         }
 
         // GET: Jokes/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Joke == null)
@@ -149,6 +156,7 @@ namespace JokeWebApp.Controllers
         }
 
         // POST: Jokes/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
